e93775f1ea5d4a174b9ddad361475aebf07273cf
Thomas Thron <tt@brandenburg-labs.com>
2023-12-07 18:23:34 +0100

    use pyproject.toml

--- setup.py
+++ setup.py
@@ -1,33 +1,3 @@
-# -*- coding: utf-8 -*-
-from setuptools import setup, find_packages
+from setuptools import setup
 
-try:
-    long_description = open("README.md").read()
-except IOError:
-    long_description = ""
-
-setup(
-    name="AlterFalter",
-    version="0.1.0",
-    description="Convolution with spectrogram display",
-    license="MIT",
-    author="Thomas Thron",
-    packages=find_packages(),
-    install_requires=[
-        "autopep8",
-        "numpy",
-        "matplotlib",
-        "SoundFile",
-        "scipy",
-        "sounddevice",
-        "pylint",
-        "numba",
-        "PySide2",
-    ],
-    long_description=long_description,
-    classifiers=[
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3.8",
-    ],
-    python_requires=">=3.8",
-)
+setup()
